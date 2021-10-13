1419a185791b20a56ec9a83af59411cba9b18af1
kishibe-rohan <devrohan2000@gmail.com>
2021-10-13 09:57:05 +0530

    added User model

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,3 +1,8 @@
+const Product = require("../models/productModel");
+const ErrorHandler = require("../utils/errorHandler");
+const catchAsyncErrors = require("../middleware/catchAsynErrors");
+const ApiFeatures = require("../utils/apiFeatures");
+
 exports.getAllProducts = (req, res) => {
   res.status(200).json({ message: "This is the Product route" });
 };
