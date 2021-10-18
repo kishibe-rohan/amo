ee61e02ed0ef3b8274db0a741b6864773585c87f
kishibe-rohan <devrohan2000@gmail.com>
2021-10-18 09:08:20 +0530

    finished homepage design

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
 import { CgMouse } from "react-icons/all";
-import "./Home.css";
 
+import ProductCard from './ProductCard';
+import logo from '../../images/logo.png'
+import "./Home.css";
+import MetaData from '../layout/MetaData';
+
+const product = {
+    name: 'Black Hoodie',
+    price:"$300",
+    _id:"skult",
+    images:[{url:"https://5.imimg.com/data5/ZZ/CZ/GG/SELLER-40281611/plain-black-hoodie-500x500.jpg"}]
+}
 const Home = () => {
   return (
     <>
+    <MetaData title="amo | Crafted With Love" />
       <div className="banner">
-          <h1>AMO</h1>
+          <img src={logo} alt="amo" />
           <p>Welcome to your one stop fashion destination</p>
 
           <a href="#container">
@@ -18,7 +29,9 @@ const Home = () => {
 
       <h2 className="home-header">Featured Fashion</h2>
       <div className="container" id="container">
-          Products
+          <ProductCard product={product} />
+          <ProductCard product={product} />
+          <ProductCard product={product} />
       </div>
     </>
   )
