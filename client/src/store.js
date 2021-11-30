6d8957acdf7a3dd00f81299dc9e35300525ff050
kishibe-rohan <devrohan2000@gmail.com>
2021-11-30 09:57:29 +0530

    user reducers completed

--- client/src/store.js
+++ client/src/store.js
@@ -7,12 +7,22 @@ import {
   productDetailsReducer,
 } from "./reducers/productReducer";
 
-import { userReducer } from "./reducers/userReducer";
+import {
+  userReducer,
+  profileReducer,
+  userDetailsReducer,
+  forgotPasswordReducer,
+  allUsersReducer,
+} from "./reducers/userReducer";
 
 const reducer = combineReducers({
   products: productsReducer,
   product: productDetailsReducer,
   user: userReducer,
+  profile: profileReducer,
+  userDetails: userDetailsReducer,
+  forgotPassword: forgotPasswordReducer,
+  users: allUsersReducer,
 });
 
 let initialState = {};
